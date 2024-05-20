85fad370404a06c6276533f089d81c5767a3c7e6
Dmitrii Nikiforov <125829393+nikkxll@users.noreply.github.com>
2024-05-20 11:55:25 +0300

    starting module 00

---
+++ cpp-module-00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnikifor <dnikifor@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 11:49:15 by dnikifor          #+#    #+#             */
+/*   Updated: 2024/05/20 11:49:36 by dnikifor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string toUpperCase(const std::string& input) {
+	std::string result = input;
+	for (char& c : result) {
+		c = std::toupper(c);
+	}
+	return result;
+}
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; ++i) {
+		std::cout << toUpperCase(argv[i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
