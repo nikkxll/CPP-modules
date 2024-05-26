d27757f33810d06931637e0dace9a85de704c5cb
Dmitrii Nikiforov <dmitr.nikiforov@gmail.com>
2024-05-26 20:26:58 +0300

    cpp02 ex02 done

--- cpp-module-02/ex02/Fixed.cpp
+++ cpp-module-02/ex02/Fixed.cpp
@@ -6,36 +6,38 @@
 /*   By: dnikifor <dnikifor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:01:34 by dnikifor          #+#    #+#             */
-/*   Updated: 2024/05/25 23:27:08 by dnikifor         ###   ########.fr       */
+/*   Updated: 2024/05/26 20:26:27 by dnikifor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 const int Fixed::fractBitsNum = 8;
+const int Fixed::fractionMask = 0xffffffff >> (32 - fractBitsNum);
+const int Fixed::wholeMask = -1 ^ fractionMask;
 
 Fixed::Fixed() : value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed& other) : value(other.value)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::Fixed(const int intValue)
 {
 	setRawBits(intValue);
 	value = intValue << fractBitsNum;
-	std::cout << "Int to fixed constructor called" << std::endl;
+	// std::cout << "Int to fixed constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float floatValue)
 {
 	setRawBits(floatValue);
 	value = static_cast<int>(roundf(floatValue * (1 << fractBitsNum)));
-	std::cout << "Float to fixed constructor called" << std::endl;
+	// std::cout << "Float to fixed constructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
@@ -44,13 +46,13 @@ Fixed& Fixed::operator=(const Fixed& other)
 	{
 		value = other.value;
 	}
-	std::cout << "Copy assignment operator called" << std::endl;
+	// std::cout << "Copy assignment operator called" << std::endl;
 	return *this;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+	// std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const
@@ -63,18 +65,153 @@ void Fixed::setRawBits(int const raw)
 	value = raw;
 }
 
-float Fixed::toFloat(void) const
+float Fixed::fixedToFloat(void) const
 {
-	return (static_cast<float>(getRawBits()) / static_cast<float>(1 << fractBitsNum));
+	return (static_cast<float>(this->getRawBits())
+		/ static_cast<float>(1 << fractBitsNum));
 }
 
-int Fixed::toInt(void) const
+int Fixed::floatToFixed(const float floatValue) const
 {
-	return (getRawBits() >> fractBitsNum);
+	return (static_cast<int>(roundf(floatValue
+		* static_cast<float>(1 << fractBitsNum))));
+}
+
+int Fixed::fixedToInt(void) const
+{
+	return (this->getRawBits() >> fractBitsNum);
+}
+
+int Fixed::intToFixed(const int intValue) const
+{
+	return (intValue << fractBitsNum);
+}
+
+int Fixed::fractionPart(void) const
+{
+	return (this->getRawBits() & fractionMask);
+}
+
+int Fixed::wholePart(void) const
+{
+	return (this->getRawBits() & wholeMask);
 }
 
 std::ostream &operator<<(std::ostream &o, const Fixed &fixed)
 {
-	o << fixed.toFloat();
+	o << fixed.fixedToFloat();
 	return o;
+}
+
+bool Fixed::operator>(const Fixed& rhs)
+{
+	return (this->value > rhs.value);
+}
+
+bool Fixed::operator<(const Fixed& rhs)
+{
+	return (this->value < rhs.value);
+}
+
+bool Fixed::operator>=(const Fixed& rhs)
+{
+	return (this->value >= rhs.value);
+}
+
+bool Fixed::operator<=(const Fixed& rhs)
+{
+	return (this->value <= rhs.value);
+}
+
+bool Fixed::operator==(const Fixed& rhs)
+{
+	return (this->value == rhs.value);
+}
+
+bool Fixed::operator!=(const Fixed& rhs)
+{
+	return (this->value == rhs.value);
+}
+
+Fixed Fixed::operator+(const Fixed& other)
+{
+	Fixed resFixed;
+
+	resFixed.value = this->value + other.value;
+	return (resFixed);
+}
+
+Fixed Fixed::operator-(const Fixed& other)
+{
+	Fixed resFixed;
+
+	resFixed.value = this->value - other.value;
+	return (resFixed);
+}
+
+Fixed Fixed::operator*(const Fixed& other)
+{
+	Fixed resFixed;
+	
+	resFixed.value = static_cast<int>((static_cast<long long>(this->value)
+		* static_cast<long long>(other.value)) >> (fractBitsNum));
+	return (resFixed);
+}
+
+Fixed Fixed::operator/(const Fixed& other)
+{
+	Fixed resFixed;
+	resFixed.value = static_cast<int>((static_cast<long long>(this->value)
+		<< fractBitsNum) / static_cast<long long>(other.value));
+	return (resFixed);
+}
+
+Fixed Fixed::operator++()
+{
+	(this->value)++;
+	return *this;
+}
+
+Fixed Fixed::operator--()
+{
+	(this->value)--;
+	return *this;
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed tmp(*this);
+	++(this->value);
+	return tmp; 
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed tmp(*this);
+	--(this->value);
+	return tmp; 
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b)
+{
+	// std::cout << "non-constant min invoked" << std::endl;
+	return (a.value > b.value) ? b : a;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
+{
+	// std::cout << "constant min invoked" << std::endl;
+	return (a.value > b.value) ? b : a;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b)
+{
+	// std::cout << "non-constant max invoked" << std::endl;
+	return (a.value > b.value) ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b)
+{
+	// std::cout << "constant max invoked" << std::endl;
+	return (a.value > b.value) ? a : b;
 }
--- cpp-module-02/ex02/Fixed.hpp
+++ cpp-module-02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: dnikifor <dnikifor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 11:01:31 by dnikifor          #+#    #+#             */
-/*   Updated: 2024/05/25 23:27:12 by dnikifor         ###   ########.fr       */
+/*   Updated: 2024/05/26 20:19:01 by dnikifor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ class Fixed {
 	private:
 		int	value;
 		static const int fractBitsNum;
+		static const int fractionMask;
+		static const int wholeMask;
+		
 	public:
 		Fixed();
 		Fixed(const int intValue);
@@ -30,8 +33,34 @@ class Fixed {
 
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
-		float toFloat(void) const;
-		int toInt(void) const;
+		float fixedToFloat(void) const;
+		int floatToFixed(const float floatValue) const;
+		int fixedToInt(void) const;
+		int intToFixed(const int intValue) const;
+		int fractionPart(void) const;
+		int wholePart(void) const;
+
+		bool operator>(const Fixed& rhs);
+		bool operator<(const Fixed& rhs);
+		bool operator>=(const Fixed& rhs);
+		bool operator<=(const Fixed& rhs);
+		bool operator==(const Fixed& rhs);
+		bool operator!=(const Fixed& rhs);
+		
+		Fixed operator+(const Fixed& other);
+		Fixed operator-(const Fixed& other);
+		Fixed operator*(const Fixed& other);
+		Fixed operator/(const Fixed& other);
+		
+		Fixed operator++();
+		Fixed operator--();
+		Fixed operator++(int);
+		Fixed operator--(int);
+		
+		static Fixed& min(Fixed& a, Fixed& b);
+		static Fixed& max(Fixed& a, Fixed& b);
+		const static Fixed& min(const Fixed& a, const Fixed& b);
+		const static Fixed& max(const Fixed& a, const Fixed& b);
 };
 
 std::ostream &operator<<(std::ostream &o, const Fixed &fixed);
