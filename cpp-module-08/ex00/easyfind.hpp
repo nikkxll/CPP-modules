539a56d14679139ca82f4da2abcb0398f07a2011
Dmitrii Nikiforov <dmitr.nikiforov@gmail.com>
2024-08-15 22:58:17 +0300

    cpp08 ex00 done

--- cpp-module-08/ex00/easyfind.hpp
+++ cpp-module-08/ex00/easyfind.hpp
@@ -6,14 +6,36 @@
 /*   By: dnikifor <dnikifor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 16:51:54 by dnikifor          #+#    #+#             */
-/*   Updated: 2024/08/15 18:33:58 by dnikifor         ###   ########.fr       */
+/*   Updated: 2024/08/15 22:51:39 by dnikifor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template <typename T>
-void easyfind(T container, int toFind)
-{
+#include <algorithm>
 
+template <typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+    {
+        throw std::runtime_error("Runtime error: element not found");
+    }
+
+    return it;
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T& container, int value)
+{
+    typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+    {
+        throw std::runtime_error("Runtime error: element not found");
+    }
+
+    return it;
 }
